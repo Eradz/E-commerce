11f474269fb710cb0b141cc463977bfb0c5fcdbd
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2025-09-25 15:00:48 +0100

    Fix: netflify push

--- netlify/functions/common/config/db.ts
+++ netlify/functions/common/config/db.ts
@@ -1,14 +1,7 @@
 import { Sequelize, DataType } from "sequelize";
 import { readFileSync } from 'node:fs'
 import dotenv from "dotenv"
-import { join } from "node:path";
 dotenv.config()
-// export const sequelize = new Sequelize( process.env.NODE_ENV === "development" ? "postgresql://postgres:351885@localhost:5432/E-commerce" : "postgres://avnadmin:AVNS_KEwRFRgALaIONmh4qzl@tech-commerce-anaguchidiebere-ebf9.j.aivencloud.com:19629/defaultdb?sslmode=require",
-//     {
-//         define: {
-//             freezeTableName: true,
-//           },
-//     })
 export const sequelize = new Sequelize({
   dialectOptions:{
     ssl:{
@@ -25,14 +18,6 @@ export const sequelize = new Sequelize({
   define:{
     freezeTableName:true
   }
-  /**
-   * host: process.env.POSTGRES_HOST,
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT,
-  dialect: process.env.POSTGRES_DIALECT,
-  database:process.env.POSTGRES_DATABASE, 
-   */
 })
 export const db = async(): Promise<void> =>{
     try {
