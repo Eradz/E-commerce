489f16f1aa5c67a99a9b64129a3ef22f093c6675
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2025-09-25 14:40:15 +0100

    Fix: netflify push

--- netlify/functions/common/config/db.ts
+++ netlify/functions/common/config/db.ts
@@ -1,14 +1,33 @@
 import { Sequelize, DataType } from "sequelize";
-
-export const sequelize = new Sequelize("postgresql://postgres:351885@localhost:5432/E-commerce",
-    {
-        define: {
-            freezeTableName: true,
-          },
-    })
+import { readFileSync } from 'node:fs'
+import { join } from "node:path";
+// export const sequelize = new Sequelize( process.env.NODE_ENV === "development" ? "postgresql://postgres:351885@localhost:5432/E-commerce" : "postgres://avnadmin:AVNS_KEwRFRgALaIONmh4qzl@tech-commerce-anaguchidiebere-ebf9.j.aivencloud.com:19629/defaultdb?sslmode=require",
+//     {
+//         define: {
+//             freezeTableName: true,
+//           },
+//     })
+export const sequelize = new Sequelize({
+  dialectOptions:{
+    ssl:{
+      ca: readFileSync('./ca.pem').toString(),
+      require:true
+    },
+  },
+  host: "tech-commerce-anaguchidiebere-ebf9.j.aivencloud.com",
+  username: "avnadmin",
+  password: "AVNS_KEwRFRgALaIONmh4qzl",
+  port: 19629,
+  dialect: "postgres",
+  database:"defaultdb", 
+  define:{
+    freezeTableName:true
+  }
+})
 export const db = async(): Promise<void> =>{
     try {
         await sequelize.authenticate();
+        await sequelize.sync()
         console.log('Connection has been established successfully.');
       } catch (error) {
         console.error('Unable to connect to the database:', error);
