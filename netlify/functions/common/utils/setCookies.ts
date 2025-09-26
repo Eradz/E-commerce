a9b6f758bcd6f36596df1391922ff408627ade38
Anagu Chidiebere Andrew <anaguchidiebere@gmail.com>
2025-09-26 15:07:42 +0100

    Fix: Hid exposed secrets

--- netlify/functions/common/utils/setCookies.ts
+++ netlify/functions/common/utils/setCookies.ts
@@ -2,14 +2,7 @@ import { Response } from "express";
 
 export const setCookies = (res: Response, name: string, value: string) => {
 
-    res.cookie(name, value, process.env.NODE_ENV === 'development' ? 
-        {
-            domain: "localhost",
-            maxAge: 1000 * 60 * 60, // 1 hour
-            sameSite: "lax",
-            secure: false,
-            httpOnly: true
-        } :
+    res.cookie(name, value, 
         {
             maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
             sameSite: "none",
